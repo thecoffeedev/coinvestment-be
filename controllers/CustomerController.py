ff4bdb9d802f70063674517472ff2b864c745c79
kurupn <kurupn@coventry.ac.uk>
2022-09-28 15:25:51 +0100

    Wallet controller and data access. Sign in flow completion until controller.

--- controllers/CustomerController.py
+++ controllers/CustomerController.py
@@ -1,4 +1,6 @@
 from data_access.CustomerDataAccess import CustomerDataAccess
+from models.Customer import Customer
+from models.Utility import  Utility
 
 
 class CustomerController:
@@ -7,6 +9,23 @@ class CustomerController:
         self.da = CustomerDataAccess(app)
         print(type(app))
 
+    def customerSignIn(self, jsonData):
+        try:
+            print("Inside customerSignIn")
+            customer = Customer()
+            customer.setEmailAddress(jsonData["emailAddress"])
+            customer.setPasswordHash(Utility.generatePasswordHash(jsonData["password"]))
+        except Exception as e:
+            print(e)
+            response = \
+                {
+                    "status":
+                        {
+                            "status code": "FAILURE",
+                            "status message": e.args[0]
+                        },
+                }
+            return response
 
 
 

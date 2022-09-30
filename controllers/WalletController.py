6f633aa04a9b62c7412a4132824462ac991c4082
kurupn <kurupn@coventry.ac.uk>
2022-09-30 18:05:21 +0100

    Added CRUD methods into WalletDataAccess.

--- controllers/WalletController.py
+++ controllers/WalletController.py
@@ -6,23 +6,9 @@ class WalletController:
 
     def __init__(self, app):
         self.WDA = WalletDataAccess(app)
+        # self.WDA.generateDayZeroData()
 
-    def dayZeroData(self):
-        # url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=false"
-        # res = requests.get(url).json()
-        #
-        # for item in currencyList:
-        #     currencyList.append([item["symbol"], item["name"]])
-        #
-        # print(currencyList)
-
-        self.WDA.generateDayZeroData()
-
-
-
-
-
-    # def createWallet(self):
-    #     self.WDA.createWallet()
-    #     print("in wallet controller")
+    def createWallet(self):
+        self.WDA.createWallet()
+        print("in wallet controller")
 
--- data_access/WalletDataAccess.py
+++ data_access/WalletDataAccess.py
@@ -1,4 +1,6 @@
 from flaskext.mysql import MySQL
+from models.Wallet import Wallet
+from models.Utility import Utility
 from decouple import config
 
 
@@ -11,8 +13,13 @@ class WalletDataAccess:
         app.config['MYSQL_DATABASE_DB'] = config('DB_NAME')
         app.config['MYSQL_DATABASE_HOST'] = config('DB_HOST')
         self.mysql.init_app(app)
-
         # self.mysqlDB = MySQL()
+
+        self.generateDayZeroData()
+        self.createWallet()
+        self.insertDayZeroWalletData()
+        self.readWalletFromWalletAddress("UazgXbwu2tloBcajCPb8")
+        self.readWalletFromCustomerID("Debo32tKqJBeZwHHgkvx")
 
     def generateDayZeroData(self):
         # Create the table if it does not exist
@@ -51,32 +58,135 @@ class WalletDataAccess:
         #checkTableExistsQuery = "SELECT * FROM information_schema.tables WHERE table_name = 'AvailableCryptocurrency'"
         #cur.execute(checkTableExistsQuery)
 
+    def testMethod(self):
+        createQuery = "SELECT * from testInsert1"
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute(createQuery)
+        myresult = cur.fetchall()
 
-"""
+        for x in myresult:
+            print(x)
+            print(x[0])
+
     def createWallet(self):
-        print("in wallet data access")
-        # # self.mysql = MySQL()
-        # # Creating a connection cursor
-        # cursor = self.mysql.connect().cursor()
-        #
-        # # Executing SQL Statements
-        # query = "CREATE TABLE test3(id VARCHAR(20), name VARCHAR(20))"
-        # cursor.execute(query)
-        #
-        # query = "INSERT INTO test3('1', 'Nikita')"
-        # cursor.execute(query)
-        #
-        # # Saving the Actions performed on the DB
-        # self.mysql.connect().commit()
-        #
-        # # Closing the cursor
-        # cursor.close()
-        # print(" Table created successfully ")
+        print("createWallet entry")
+        createTableQuery = "CREATE TABLE IF NOT EXISTS Wallet (" \
+                           "WalletAddress VARCHAR(20), " \
+                           "CustomerID VARCHAR(20), " \
+                           "InitialBalance FLOAT(53), " \
+                           "CurrentBalance FLOAT(53), " \
+                           "CryptocurrencyCode VARCHAR(10), " \
+                           "HoldingPeriod INT(5) " \
+                           ")"
 
-        conn = self.mysqlDB.connect(MYSQL_DATABASE_USER='root', MYSQL_DATABASE_PASSWORD='', MYSQL_DATABASE_HOST='127.0.0.1')
-        cursorDB = conn.cursor()
-        sql = "CREATE database MYDATABASE"
-        cursor.execute(sql)
-        conn.close()
-        print(" DB created successfully ")
-        """
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute(createTableQuery)
+        cur.close()
+        con.close()
+        print("createWallet exit")
+
+
+    def insertDayZeroWalletData(self):
+        print("insertDayZeroWalletData entry")
+        con = self.mysql.connect()
+        cur = con.cursor()
+
+        # If the table exists and is empty, insert the customer models
+        cur.execute("select exists (SELECT * FROM Wallet)")
+        # result = cur.fetchall()
+        # print("result ", result)
+        # print("result one ", cur.fetchone()[0])
+
+        if not cur.fetchone()[0]:
+            print("result not exists")
+            walletOne = Wallet()
+            walletOne.setCustomerID("1WNJKpBpYfWwKIlvbaz0")
+            walletOne.setWalletAddress(Utility.generateRandomID())
+            print("wallet address ", walletOne.getWalletAddress())
+            walletOne.setInitialBalance(8432.2871)
+            walletOne.setCurrentBalance(8432.2871)
+            walletOne.setCryptocurrencyCode('btc')
+            walletOne.setHoldingPeriod(24)
+            self.insertWallet(walletOne)
+
+            walletOne = Wallet()
+            walletOne.setCustomerID("Debo32tKqJBeZwHHgkvx")
+            walletOne.setWalletAddress(Utility.generateRandomID())
+            print("wallet address ", walletOne.getWalletAddress())
+            walletOne.setInitialBalance(2731.3329)
+            walletOne.setCurrentBalance(2731.3329)
+            walletOne.setCryptocurrencyCode('trx')
+            walletOne.setHoldingPeriod(10)
+            self.insertWallet(walletOne)
+        print("insertDayZeroWalletData exit")
+
+
+    def insertWallet(self, walletObj):
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute("INSERT INTO Wallet (WalletAddress, CustomerID, InitialBalance, CurrentBalance, " \
+                    "CryptocurrencyCode, HoldingPeriod) VALUES(%s, %s, %s, %s,  %s, %s)",
+                    (walletObj.getWalletAddress(), walletObj.getCustomerID(), walletObj.getInitialBalance(),
+                    walletObj.getCurrentBalance(), walletObj.getCryptocurrencyCode(), walletObj.getHoldingPeriod()))
+        cur.close()
+        con.commit()
+        con.close()
+
+    def readWalletFromWalletAddress(self, walletObj):
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Wallet where WalletAddress = '"+walletObj.getWalletAddress()+"'")
+
+        result = cur.fetchall()
+        cur.close()
+        con.commit()
+        con.close()
+
+        for wallet in result:
+            print(wallet)
+            walletOne = Wallet()
+            walletOne.setCustomerID(wallet[1])
+            walletOne.setWalletAddress(wallet[0])
+            print("wallet address ", wallet[0])
+            walletOne.setInitialBalance(wallet[2])
+            walletOne.setCurrentBalance(wallet[3])
+            walletOne.setCryptocurrencyCode(wallet[4])
+            walletOne.setHoldingPeriod(wallet[5])
+            print(walletOne)
+
+    def readWalletFromCustomerID(self, walletObj):
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Wallet where customerID = '"+walletObj.getCurrentBalance()+"'")
+
+        result = cur.fetchall()
+        cur.close()
+        con.commit()
+        con.close()
+
+        walletsList = list()
+        for wallet in result:
+            print(wallet)
+            walletOne = Wallet()
+            walletOne.setCustomerID(wallet[1])
+            walletOne.setWalletAddress(wallet[0])
+            walletOne.setInitialBalance(wallet[2])
+            walletOne.setCurrentBalance(wallet[3])
+            walletOne.setCryptocurrencyCode(wallet[4])
+            walletOne.setHoldingPeriod(wallet[5])
+            print(walletOne.__dict__)
+            walletsList.append(walletOne)
+        print(walletsList)
+
+    def updateWalletCurrentBalance(self, walletObj):
+        con = self.mysql.connect()
+        cur = con.cursor()
+        cur.execute("UPDATE Wallet set CurrentBalance = '"+walletObj.getCurrentBalance()+
+                    "' where WalletAddress = '"+walletObj.getWalletAddress()+"'")
+
+        result = cur.fetchall()
+        cur.close()
+        con.commit()
+        con.close()

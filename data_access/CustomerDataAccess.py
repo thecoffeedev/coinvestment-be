da22a9a86a156a2b08cfb4c2f5b87ab6e133691b
sandhu63 <sandhu63@coventry.ac.uk>
2022-10-04 18:25:23 +0100

    removed generating day zero data from CDA constructor

--- data_access/CustomerDataAccess.py
+++ data_access/CustomerDataAccess.py
@@ -13,15 +13,6 @@ class CustomerDataAccess:
         app.config['MYSQL_DATABASE_DB'] = config('DB_NAME')
         app.config['MYSQL_DATABASE_HOST'] = config('DB_HOST')
         self.mysql.init_app(app)
-        self.createTables()
-        self.insertDayZeroData()
-
-        c = Customer()
-        c.setCustomerID("1WNJKpBpYfWwKIlvbaz0")
-        c.setCurrentSignInDatetime(12345)
-        c.setPreviousSignInDatetime(67890)
-        self.updateCustomerSignInDatetimes(c)
-
 
     def createTables(self):
         createTableQuery = "CREATE TABLE IF NOT EXISTS Customer (" \
